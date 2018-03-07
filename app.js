506c8f999759738f203242dbb7fc4c84ac96be43
proluk <lukaszprokopiuk@gmail.com>
2018-03-07 09:21:16 +0100

    better scheduler

--- app.js
+++ app.js
@@ -23,7 +23,19 @@ io.listen(process.env.SOCKETS_PORT);
 console.log(chalk.hex('#00F900')('Sockets are listening on port: ' + process.env.SOCKETS_PORT));
 
 // let mailer = new Mail(nodemailer, chalk);
-
+Scheduler.startServiceTest( () => {
+    let promises = [];
+    Service.find({}, (err, res) => {
+        res.forEach((service) => {
+            promises.push(runTestServiceStatus(service));
+        });
+        Promise.all(promises).then((data) => {
+            Service.find({}, (err, res) => {
+                io.sockets.emit("servicelist", res);
+            });
+        });
+    });
+});
 Service.find({}, (err, res) => {
     res.forEach((service) => {
         runTestServiceStatus(service);
@@ -67,41 +79,33 @@ io.on('connection', (client) => {
 
 });
 
-function runTestServiceStatus(service) {
-    console.log("Run test service: " + chalk.hex('#FF0572')(service.getName()) );
-    request.get(service.getUrl()).on('error', function (error) {
-        service.setStatus(error, (updatedService) => {
-            io.sockets.emit('updateStatus', {
-                updatedService
+const runTestServiceStatus = (service) => {
+    return new Promise((resolve, reject) => {
+        console.log("Run test service: " + chalk.hex('#FF0572')(service.getName()) );
+        request.get(service.getUrl()).on('error', function (error) {
+            service.setStatus(error, (updatedService) => {
+                console.log("Error for service: "+chalk.hex("#FF0527")(service.getName()) );
+                mailer.sendErrorMail(updatedService);
             });
-            console.log("Error for service: "+chalk.hex("#FF0527")(service.getName()) );
-            mailer.sendErrorMail(updatedService);
-        });
-        Service.findOneAndUpdate({
-            id: service.getId()
-        }, service, {}, function (err) {
-            if (err) console.log(err);
-        });
-        setTimeout(() => {
-            runTestServiceStatus(service);
-        }, 1800000);
-    }).on('response', function (response) {
-        service.setStatus(response.statusCode, (updatedService) => {
-            io.sockets.emit('updateStatus', {
-                updatedService
+            Service.findOneAndUpdate({
+                id: service.getId()
+            }, service, {}, function (err) {
+                if (err) console.log(err);
+                reject(error);
             });
-            console.log(
-                "Test end for service: "+chalk.hex("#FF0527")(service.getName())+
-                " Status: "+chalk.hex("#05B305")(response.statusCode)+
-                " Time: "+chalk.hex("#E0DD06")(new Date().toLocaleString()) );
-        });
-        Service.findOneAndUpdate({
-            id: service.getId()
-        }, service, {}, function (err, test) {
-            if (err) console.log(err);
-        });
-        setTimeout(() => {
-            runTestServiceStatus(service);
-        }, 1800000);
+        }).on('response', function (response) {
+            service.setStatus(response.statusCode, (updatedService) => {
+                console.log(
+                    "Test end for service: "+chalk.hex("#FF0527")(service.getName())+
+                    " Status: "+chalk.hex("#05B305")(response.statusCode)+
+                    " Time: "+chalk.hex("#E0DD06")(new Date().toLocaleString()) );
+            });
+            Service.findOneAndUpdate({
+                id: service.getId()
+            }, service, {}, function (err, test) {
+                if (err) console.log(err);
+                resolve("success");
+            });
+        });        
     });
 }
--- modules/Schedule.js
+++ modules/Schedule.js
@@ -6,10 +6,17 @@ class Schedule {
         this.dailyMailRule.dayOfWeek = [new schedule.Range(0, 6)];
         this.dailyMailRule.hour = 6;
         this.dailyMailRule.minute = 0;
+        this.serviceTestRule = new schedule.RecurrenceRule();
+        this.serviceTestRule.minute = new schedule.Range(0, 59, 1);
     }
     startDailyMail(func) {
         this.dailyMail = schedule.scheduleJob(this.dailyMailRule, () => {
             func();
+        });
+    }
+    startServiceTest(callback){
+        this.serviceTest = schedule.scheduleJob(this.serviceTestRule, () => {
+            callback();
         });
     }
 }
